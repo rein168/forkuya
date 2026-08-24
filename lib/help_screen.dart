002beaa0bdf6cb2510b31f56f7d5a42f142161ea
Xavier <xavie@example.com>
2026-08-24 09:09:34 +0700

    Migrate to flutter_markdown_plus (flutter_markdown is discontinued)

--- lib/help_screen.dart
+++ lib/help_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 
 class HelpScreen extends StatelessWidget {
   const HelpScreen({super.key});
